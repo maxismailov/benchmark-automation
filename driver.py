f0fcfa4dbc870d0dd77492834025d4953d6ab11d
Ismailov <maxisma@3c22fb44e639.ant.amazon.com>
2020-07-17 17:41:31 -0600

    Refactored things to write all output to a single master file

--- driver.py
+++ driver.py
@@ -2,13 +2,13 @@ import argparse
 import os
 import sys
 import subprocess
-import perfparser
 
 def parse_all_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("input_dir",help="Path to the directory that contains our lmp_mpi executable, bash script, and in.* files")
     parser.add_argument("--node_list",nargs="*",type=int,default=[])
     parser.add_argument("--task_list",nargs="*",type=int,default=[])
+    parser.add_argument("--out-_ile",type=str,default="")
     #TODO: Make an option flag to possibly scale the size of the problem along with the number of clusters. Currently we keep the problem size the same always
     return parser.parse_args()
 
@@ -19,19 +19,25 @@ def main(argv):
     node_list = args.node_list
     task_list = args.task_list
     input_dir = args.input_dir
+    out_file = args.out_file
     if not os.path.exists(input_dir):
         print("Error: the input directory you specified doesn't exist")
         exit(-1)
 
-    if not node_list or not task_list:
+    if not node_list or not task_list or not out_file:
         print("Error: One of the essential lists (node_list or task_list) were not provided. Please provide a list of values to proceed")
         exit(-1)
 
-    # TODO: Possibly write this output to our aggregated output file?
+    # TODO: Make another file that contains #node information for each job that gets run, so we can have that information in our ultimate aggregated file 
     print("LAMMPS Simulation started with parameters: dir=", args.input_dir, "nodes list=", node_list, "tasks=", task_list)
     print(args.input_dir)
     print(node_list)
     print(task_list)
+
+    ret = subprocess.call(["cp","get_perf_data.sh",input_dir])
+    if ret != 0:
+        print("Error with copying over our execution script")
+        exit(-1)
 
     job_list = []
     
@@ -40,14 +46,18 @@ def main(argv):
         for ntasks in task_list:
             node_command = "--nodes=" + str(nodes)
             tasks_command = "--ntasks-per-node=" + str(ntasks)
-            proc = subprocess.Popen(["sbatch",node_command, tasks_command, "./jxu_auto.sh"],stdout=subprocess.PIPE,cwd=input_dir)
+            # TODO: Add in a master aggregated file that all of our jobs will write into
+            out_file_command = "--output="+out_file
+            file_mode_command = "--open-mode=append"
+            proc = subprocess.Popen(["sbatch",node_command, tasks_command, out_file_command, file_mode_command, "./get_perf_data.sh"],stdout=subprocess.PIPE,cwd=input_dir)
             job_str = str(proc.communicate()[0])
             job_num = job_str.split(' ')[3].replace("\\n","").replace("'","")
             job_list.append(job_num)
-            
+
     # At this point job_list is populated with the job numbers of all of our submitted jobs
     # TODO: Make another python module that we pass off the job numbers to, and that actually parses our output files
-    perfparser.parse_jobs(job_list)
+    
+
 
 
 if __name__ == "__main__":
