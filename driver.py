b8f39c5cca2b3f0e7b26ca281ff54dab448e7705
AWS ParallelCluster user <ec2-user@ip-172-16-2-208.ec2.internal>
2020-07-17 18:42:06 +0000

    Initial commit to dev branch

---
+++ driver.py
@@ -0,0 +1,14 @@
+import argparse
+import numpy
+import os
+import sys
+import subprocess
+
+def parse_all_args():
+    parser = argparse.ArgumentParser()
+
+def main():
+    print("Hello world!")
+
+if __name__ == "__main__":
+    main(sys.argv)
