a3b649be2aa00fa838ceed9a23b919256f0ed3e5
AWS ParallelCluster user <ec2-user@ip-172-16-2-208.ec2.internal>
2020-07-22 03:26:54 +0000

    Have parsing successfully populating our tabular_data object, now running into issues with printing to file

--- perfparser.py
+++ perfparser.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import subprocess
-import asyncio
 import csv
 import numpy as np
 
@@ -17,106 +16,107 @@ import numpy as np
 
 
 # TODO: Write data to master CSV file --> probably where we would need to write lock
+# - WE NEED TO INCLUDE A SETUP.PY TO INSTALL ALL DEPENDENCIES
 
 
 # Possibly use portalocker package if we need to implement locks
 def main(argv):
     # Get the job file from the uuid given as argv[1]
-
     ticket = argv[1]
+
     # We will eventually want to make this a non hardcoded-value
 
-    temp_file = open("/tmp/temp-bench-auto.txt","r")
-
+    temp_file = open("/software/benchmarks/utah/AWS-EBS-c5n.18xlarge-EFA/temp-bench-auto.txt","r")
     
 
     # Find the job number from the uuid we were given
-
     job_num = -1
+    out_file_name = ""
 
     for line in temp_file:
-
-        tokens = line.split(" ")
-
+        if not out_file_name:
+            out_file_name = line
+        tokens = line.split()
         if tokens[0] == ticket:
-
             job_num = int(tokens[1])
-
             break
+    
+    print("I am uuid: ", ticket , " and my job number is: ", job_num)
         
-        
-        
-    job_file_name = "slurm-"+job_num+".out"
+    job_file_name = "slurm-"+str(job_num)+".out"
     job_file = open(job_file_name,"r")
 
-
     slurm_out_lines = []
-
     for line in job_file:
-
         slurm_out_lines.append(line)
 
     # Get our index in the right spot by looking for our special sentinel value
-
     index_line = 0
-
     for line in slurm_out_lines:
-
         index_line += 1
-
         if line.count("~") != 0:
-
             break
-# At this point index_line will be set to the begining of the relevant data
-        
 
+    print("First line index: ", index_line , " First line: " , slurm_out_lines[index_line] , "\n")
     
-
+    # At this point index_line will be set to the begining of the relevant data
     # *** Schema for tabular_data:
     #   Nodes, Tasks, Atoms, Type, Time, Has_EFA, Steps, Iteration, Job ID
 
-    tabular_data = np.array((1,9)) # starting array   
-
-    index_data = 0
-    nodes = slurm_out_lines[1].count(",") + 1 # the nodes list is separated by commas. Number of nodes = num commas + 1
-    ntasks = line[index_line].split(" ")[5] # get ntasks
-    index_line += 1 # move down a line
-    num_atoms = line[index_line].split(" ")[11] # get number of atoms
+    # Sets up the header for our tabular_data
+    tabular_data = np.empty((1,9),dtype='object') 
+    tabular_data[0,:] = ["Nodes", "Tasks", "Atoms", "Type", "Time", "Has_EFA", "Steps", "Iteration", "Job_ID"]
+    new_row = np.empty((1,9),dtype='object')
+    tabular_data = np.append(tabular_data, new_row,axis=0)
+    index_data = 1
+    nodes = slurm_out_lines[1].count(",") + 1 
+    print(slurm_out_lines[index_line].split())
+    ntasks = slurm_out_lines[index_line].split()[6] 
+    index_line += 1
+    num_atoms = slurm_out_lines[index_line].split()[11] 
 
 
     index_line -= 1 # move back up before we start the while loop
 
+    # The '~' character is the sentinel character for the end of our run
+    while slurm_out_lines[index_line].count("~") == 0: 
+       # Constants for all runs: num_nodes, num_tasks, num_atoms, job id
+        print("Current line: ", slurm_out_lines[index_line])
+        tabular_data[index_data,0] = nodes 
+        tabular_data[index_data,1] = ntasks 
+        tabular_data[index_data,2] = num_atoms 
+        tabular_data[index_data,8] = job_num
 
-    while slurm_out_lines.count("~") == 0: # while the ~ count is 0 (which denotes the location of the relevant data) fill our data table
+        # Variables from data line 1: iter, type, EFA 
+        tabular_data[index_data,7] = slurm_out_lines[index_line].split()[3] 
+        tabular_data[index_data,3] = slurm_out_lines[index_line].split()[8] 
+        tabular_data[index_data,5] = slurm_out_lines[index_line].split()[0] 
+
+        index_line += 1 
+
+        # Variables from data line 2: loop time, steps
+        tabular_data[index_data,4] = slurm_out_lines[index_line].split()[3] 
+        tabular_data[index_data,6] = slurm_out_lines[index_line].split()[8] 
+
+        print(tabular_data)
+        print("###################")
+        new_row = np.empty((1,9),dtype='object')
+
+        tabular_data = np.append(tabular_data, new_row, axis = 0)
         
-       # constants: num_nodes, num_tasks, num_atoms, job id
-        tabular_data[index_data][0] = nodes #fill tasks
-        tabular_data[index_data][1] = ntasks #fill tasks
-        tabular_data[index_data][2] = num_atoms #fill num atoms
-        tabular_data[index_data][8] = job_num
-
-        # variables from data line 1: iter, type, EFA
-        tabular_data[index_data][7] = slurm_out_lines[index_line].split(" ")[3] #grab iter number
-        tabular_data[index_data][3] = slurm_out_lines[index_line].split(" ")[8] # grab the simulation type
-        tabular_data[index_data][5] = slurm_out_lines[index_line].split(" ")[0] # Grab With or NO EFA
-
-        index_line += 1 # move down a line
-
-        # variables from data line 2: loop time, steps
-        tabular_data[index_data][4] = slurm_out_lines[index_line].split(" ")[3] # grab loop time
-        tabular_data[index_data][6] = slurm_out_lines[index_line].split(" ")[8] # grab number of steps
-      
-
-       
-        new_row = np.array((1,9))
-
-        tabular_data = np.append(tabular_data, new_row, axis = 2)
-        
-        index_data += 1 # move to next table row
-
+        # Increment row pointer and line pointer 
+        index_data += 1 
         index_line += 2 # move to next simulation
 
-    np.savetxt("master_data.csv", tabular_data, delimiter=",") # save our data to a csv file named master_data.csv
+    #dbg_file = open("debug-slurm-"+job_num+".out")
+
+    np.savetxt(out_file_name, tabular_data, delimiter=",") 
+    # Actually write this data out to our master file
+    # with open(out_file_name, 'wb') as master_file:
+    #     np.savetxt(master_file, tabular_data, delimiter=",") 
+
+if __name__ == "__main__":
+    main(sys.argv)
 
 
         
