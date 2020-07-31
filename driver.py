3e939fb4b57dcfc283536cee14387a05ee539d0a
EC2 Default User <ec2-user@ip-172-31-75-121.ec2.internal>
2020-07-31 02:21:23 +0000

    Changed the driver code to run on one local instance

--- driver.py
+++ driver.py
@@ -3,6 +3,7 @@ import os
 import sys
 import subprocess
 import uuid
+import perfparser
 
 # Authors: Max Ismailov and Caleb Grode
 # Description: A script to launch a series of slurm jobs with various numbers of nodes and ntasks 
@@ -11,16 +12,13 @@ import uuid
 #
 
 
-
 def parse_all_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("input_dir",help="Path to the directory that contains our lmp_mpi executable, bash script, and in.* files")
-    parser.add_argument("--node_list",nargs="*",type=int,default=[])
-    parser.add_argument("--task_list",nargs="*",type=int,default=[])
+    parser.add_argument("--gpu_num",type=int,default=0)
     parser.add_argument("--out_file",type=str,default="",help="The name of the file that you want all of the timing data to be aggregated into")
     parser.add_argument("--task_sequence",type=str,default="0,0")
-    # TODO: Make an option flag to scale the size of the problem along with the number of clusters. something like --size [x y z]. Also number_iterations
-    # TODO: Add the option to choose output file format: csv, tsv 
+    
     return parser.parse_args()
 
 # TODO: 
@@ -31,8 +29,7 @@ def parse_all_args():
 def main(argv):
     # Get the core arguments necessary for this to run
     args = parse_all_args()
-    node_list = args.node_list
-    task_list = args.task_list
+    gpu_num = args.gpu_num
     input_dir = args.input_dir
     out_file = args.out_file
     sequence = args.task_sequence.split(",")
@@ -43,53 +40,52 @@ def main(argv):
         print("Error: the input directory you specified doesn't exist")
         exit(-1)
 
-    if (not node_list or not task_list or not out_file) and (sequence[1]=="0"):
-        print("Error: One of the essential lists (node_list or task_list) were not provided. Please provide a list of values to proceed")
+    if (not gpu_num or not out_file) and (sequence[1]=="0"):
+        print("Error: One of the essential lists (gpu_num or task_list) were not provided. Please provide a list of values to proceed")
         exit(-1)
 
     # TODO: Make another file that contains #node information for each job that gets run, so we can have that information in our ultimate aggregated file 
-    print(" *** Batch LAMMPS Simulation started with parameters: dir=", args.input_dir, "nodes list=", node_list, "tasks=", task_list, " ***")
+    print(" *** Batch LAMMPS Simulation started with parameters: dir=", args.input_dir, "gpu num=", gpu_num, "tasks=", sequence[0])
 
-    if sequence[1] == "0":
-        ret = subprocess.call(["cp","get_perf_data.sh",input_dir])
-        if ret != 0:
-            print("Error with copying over our execution script")
-            exit(-1)
+    
+    ret = subprocess.call(["cp","get_perf_data.sh",input_dir])
+    if ret != 0:
+        print("Error with copying over our execution script")
+        exit(-1)
 
     # Make a temporary file in /tmp using some uuid, and in that file store the name of the master-out-file
-    temp_file_out_name = "/software/benchmarks/utah/AWS-EBS-c5n.18xlarge-EFA/.temp-bench-auto.txt"
+    temp_file_out_name = "/mnt/efs/in-files/.temp-bench-auto.txt"
     temp_file_out = open(temp_file_out_name,"w")
     temp_file_out.write(out_file + "\n")
 
-    job_list = []
+    # Export the necessary environment variables and call get_perf_data
+    seq_curr_str = "seq_curr="+str(int(sequence[0])+1)
+    seq_max_str = "seq_max="+sequence[1]
+    ticket = str(uuid.uuid1())
+    ticket_str = "uuid="+ticket
+    # foo = subprocess.call(["export",seq_curr_str])
+    # foo = subprocess.call(["export",seq_max_str])
+    # foo = subprocess.call(["export",ticket_str])
+    
+    job_filename = "gpu_"+str(gpu_num)+"_"+str(sequence[0])+".out"
+    job_file = open(job_filename,"w")
+    temp_file_out.write(ticket + " " + job_filename+ "\n")
 
-    if int(sequence[0]) == 0:
-        # Launch all of the requested jobs, capture the job number of each, append it 
-        for nodes in node_list:
-            for ntasks in task_list:
-                node_command = "--nodes=" + str(nodes)
-                tasks_command = "--ntasks-per-node=" + str(ntasks)
-                ticket = str(uuid.uuid1())
-                params_command = "--export=ALL,uuid="+ticket+",parse_dir="+os.getcwd()+",seq_curr=0"
-                proc = subprocess.Popen(["sbatch",node_command, tasks_command, params_command, "./get_perf_data.sh"],stdout=subprocess.PIPE,cwd=input_dir)
-                job_str = str(proc.communicate()[0])
-                job_num = job_str.split(' ')[3].replace("\\n","").replace("'","")
-                # Write to file <ticket, job_num>
-                job_list.append(job_num)
-                temp_file_out.write(ticket + " " + job_num + "\n")
-    else:
-        print("Executing sequence code!")
-        node_command = "--nodes=1"
-        tasks_command = "--ntasks-per-node=" + sequence[0]
-        ticket = str(uuid.uuid1())
-        # Had to hacky-y hardcode the value of the parse_dir...
-        params_command = "--export=ALL,uuid="+ticket+",parse_dir=/software/benchmarks/benchmark-automation/"+",seq_curr="+str(int(sequence[0])+1)+",seq_max="+sequence[1]
-        proc = subprocess.Popen(["sbatch",node_command, tasks_command, params_command, "./get_perf_data.sh"],stdout=subprocess.PIPE,cwd=input_dir)
-        job_str = str(proc.communicate()[0])
-        job_num = job_str.split(' ')[3].replace("\\n","").replace("'","")
-        # Write to file <ticket, job_num>
-        job_list.append(job_num)
-        temp_file_out.write(ticket + " " + job_num + "\n")
+    #TODO: Use sstdout=*** to redirect this output to stdout, call proc.wait() and then you can manually fire off the parser in this script!
+    print("Launching get_perf_data...")
+    proc = subprocess.Popen(["./get_perf_data.sh",str(gpu_num),sequence[0],ticket,str(int(sequence[0])+1),sequence[1]],cwd=input_dir,stdout=job_file)
+    ret = proc.wait()
+    print("Done!")
+    perfparser.parse_out_file(job_filename)
+    print("Parsing completed!")
+    # # Had to hacky-y hardcode the value of the parse_dir...
+    # params_command = "--export=ALL,uuid="+ticket+",parse_dir=/software/benchmarks/benchmark-automation/"+",seq_curr="+str(int(sequence[0])+1)+",seq_max="+sequence[1]
+    # proc = subprocess.Popen(["sbatch",node_command, tasks_command, params_command, "./get_perf_data.sh"],stdout=subprocess.PIPE,cwd=input_dir)
+    # job_str = str(proc.communicate()[0])
+    # job_num = job_str.split(' ')[3].replace("\\n","").replace("'","")
+    # # Write to file <ticket, job_num>
+    # job_list.append(job_num)
+    # temp_file_out.write(ticket + " " + job_num + "\n")
 
 
 if __name__ == "__main__":
